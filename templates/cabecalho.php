991f8e7f550dc243e3df5e4ac297f896235f4920
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-05-19 20:49:09 -0300

    login a terminar

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <!-- js boostrap adicionei a propriedade defer para garantir o carregamento so no final -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
+
+    <link rel="stylesheet" href="/guia_brecho/css/style.css">
 </head>
 
 <body>
