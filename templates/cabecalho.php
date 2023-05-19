eda9b612a6f2f0fdb322878e819c583ae4e26584
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-05-19 20:49:09 -0300

    login a terminar

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guia Brechó</title>
     <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </head>
 <body>
     <header>
