38ab1ec1943677d0c648e8b663846b4a8acdac61
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-05-19 20:49:09 -0300

    login a terminar

--- views/login.php
+++ views/login.php
@@ -1,2 +1,26 @@
-aqui coloquem as views do sistema
-apagar esse arquivo assim q colocar os primeiros arquivos nessa pasta
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/afbrecho/guia_brecho/templates/cabecalho.php";
+?>
+
+<div>
+        <form action="" autocomplete="off" method="POST">
+            <fieldset>
+                <h1>Login</h1>
+                <p>Digite os seus dados de acesso nos campos abaixo.</p>
+                <div style="display: flex; align-items: center;">
+                    <input type="mail" name="nome" id="nome" placeholder="E-mail" required autofocus>
+                </div>
+                <div style="display: flex; align-items: center;">
+                    <input type="password" name="senha" id="senha" placeholder="Senha" required>
+                </div>
+                <button type="submit">Acessar</button>
+                <p><a href="esqueci_senha.html">Esqueci minha senha</a></p> 
+                <p><a href="cadastrar.html">Cadastrar</a></p> 
+
+            </fieldset>
+         </form>
+</div>
+
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/afbrecho/guia_brecho/templates/rodape.php";
+?>
