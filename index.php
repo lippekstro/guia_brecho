4bf81d2d177b28c2df0189847fcce8a5bb3de90b
Gordadomar <juliana.marques.coelho@gmail.com>
2023-06-09 20:04:20 -0300

    9-6

--- index.php
+++ index.php
@@ -15,7 +15,4 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
 
 <?php 
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
-
-
-
-?>
+?
