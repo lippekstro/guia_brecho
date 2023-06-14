42a3ef6ccf945ed99587b35adbfa6d501405278f
HandrewEspectacao <handrewes.99@gmail.com>
2023-06-14 20:31:20 -0300

    AAA

---
+++ js/eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var dura = 200;
+    var instances = M.Carousel.init(elems);
+   
+
+  });
