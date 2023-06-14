9b1208eb9593c099352de37de852e21a31b17d6c
HandrewEspectacao <handrewes.99@gmail.com>
2023-06-14 20:31:20 -0300

    AAA

--- views/eventos.php
+++ views/eventos.php
@@ -1,8 +1,20 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
 ?>
-
-
+<hr class="hr">
+<div class="float">
+  <div class="larg" class="aa">
+    <h2 >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae laborum placeat labore iusto illo mollitia. Debitis architecto quaerat deleniti nemo soluta
+    </h2>
+    <p>local</p>
+    <p>hora</p>
+  </div>
+  <div class="larg">
+  <img class="tam_img" src="/guia_brecho/img/img.png" alt="" srcset="" >
+  </div>
+</div>
+<hr class="hr">
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
 ?>
