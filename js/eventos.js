5c7f173ae70b548c59a869adfb5de778c5c5c59e
HandrewEspectacao <handrewes.99@gmail.com>
2023-06-15 20:26:55 -0300

    implementei um carrosel do bootstrap

--- js/eventos.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var dura = 200;
-    var instances = M.Carousel.init(elems);
-   
-
-  });
