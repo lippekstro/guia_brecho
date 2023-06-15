bd690b8c7bb150abefd6718f39d50e4efc2a0934
HandrewEspectacao <handrewes.99@gmail.com>
2023-06-15 20:26:55 -0300

    implementei um carrosel do bootstrap

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guia Brechó</title>
     <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
-    <link rel="stylesheet" href="../css/style.css">
-    
+  <link rel="stylesheet" href="/guia_brecho/css/bootstrap.css">
+  <link rel="stylesheet" href="../css/style.css">
+  <script src="/guia_brecho/js/bootstrap.bundle.js" defer ></script>
 </head>
 <body>
     <header>
