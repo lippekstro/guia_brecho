e038ef3a7f31af5e42488cc15e7e6892e667238f
Felippe Fialho <felippefialho@hotmail.com>
2023-06-17 12:46:34 -0300

    alt o html dos cards e modal

--- db/banco.sql
+++ db/banco.sql
@@ -7,7 +7,7 @@ CREATE TABLE usuario (
     email VARCHAR(255) NOT NULL,
     cpf_cnpj BIGINT UNIQUE NOT NULL, 
     nivel_acesso int DEFAULT 1
-)
+);
 
 CREATE TABLE loja (
     id_loja int PRIMARY KEY AUTO_INCREMENT, 
@@ -18,7 +18,7 @@ CREATE TABLE loja (
     logo_loja LONGBLOB,
     id_usuario int NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-)
+);
 
 CREATE TABLE produto (
     id_produto int PRIMARY KEY AUTO_INCREMENT, 
@@ -30,7 +30,7 @@ CREATE TABLE produto (
     imagem_produto LONGBLOB, 
     id_loja int NOT NULL,
     FOREIGN KEY (id_loja) REFERENCES loja(id_loja)
-)
+);
 
 CREATE TABLE evento (
     id_evento int PRIMARY KEY AUTO_INCREMENT,  
@@ -38,5 +38,5 @@ CREATE TABLE evento (
     horario TIME NOT NULL, 
     local_evento VARCHAR(255) NOT NULL,
     descricao_evento TEXT NOT NULL
-)
+);
 
