a4bb5f093d744e5a7b9be58cab545c56baad6b37
André Santos <af4felipedutra@gmail.com>
2023-06-17 20:13:22 -0300

    texte rodapé

--- templates/rodape.php
+++ templates/rodape.php
@@ -1,7 +1,11 @@
 </main>
 
     <footer>
-        
+        <ul>
+            <li><p>Guia Brecho | CNPJ: 05754869/0001-45 - I.E. 78.289.821</p></li>
+            <li><p> Rua do Giz -São Luís - CEP: 22221-070 <p></li>
+            <li><a href="contato@guiabrechoslz.com.br" class="a">contato@guiabrechoslz.com.br</a></li>
+        </ul>   
     </footer>
 </body>
 </html>
