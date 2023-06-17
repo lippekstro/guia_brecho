f2f247786a3f676d4d064ec27e7b21e4c092cade
André Santos <af4felipedutra@gmail.com>
2023-06-17 15:34:51 -0300

    conclusão quemsomos

---
+++ views/quemsomos.php
@@ -0,0 +1,33 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php"
+?>
+
+<div class="caixa-laranja" id="margem">
+    <p>
+        Bem-vindo(a) ao Guia Brechó, o seu guia online para explorar o mundo dos brechós em São Luís do Maranhão! Nós acreditamos na valorização do que é feito com cuidado e no apoio aos brechozeiros e brechozeiras locais que buscam crescer e expandir seus negócios.
+        Como funciona o Guia Brechó? É simples! Nós fornecemos aos brechós locais uma plataforma para expor seus produtos, mostrando suas peças exclusivas e oferecendo informações detalhadas sobre suas lojas. Você poderá encontrar uma variedade de roupas, acessórios e itens de moda em nosso site, tudo em um só lugar!
+    </p>
+</div>
+
+<br>
+
+<div class="caixa-laranja">
+    <p>
+        Ao navegar pelo Guia Brechó, você terá acesso a uma seleção cuidadosamente curada de brechós em São Luís do Maranhão, onde poderá encontrar produtos únicos e de qualidade, contribuindo para uma moda mais sustentável e consciente. 
+    </p>
+</div>
+
+<br>
+
+<div class="caixa-laranja">
+    <p>
+        Estamos comprometidos em promover o crescimento dos brechós locais e ajudá-los a alcançar um maior número de clientes, enquanto proporcionamos aos apreciadores de moda consciente uma experiência única de descoberta e valorização de produtos únicos e especiais.
+        Junte-se a nós no Guia Brechó e faça parte dessa comunidade que valoriza a moda com cuidado, sustentabilidade e estilo. Explore os brechós locais, descubra peças exclusivas e contribua para um consumo mais consciente e responsável. Vamos juntos promover a moda sustentável e apoiar o crescimento dos brechós em São Luís do Maranhão!
+    </p>
+</div>
+
+<br>
+
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php"
+?>
