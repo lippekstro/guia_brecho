900e42d654896c71da051e4986e2d4f258d71a05
André Santos <af4felipedutra@gmail.com>
2023-06-17 18:53:08 -0300

    Criação do menu global

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -8,10 +8,24 @@
     <link rel="stylesheet" href="/guia_brecho/css/style.css">
     <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> <!--Variable icon font-->
 </head>
 <body>
     <header>
-
+        <nav>
+            <ul>
+                <li> <img src="/guia_brecho/img/logo_guia_brecho-rem.png" class="logowidth"> </li>
+              <li><a href="/guia_brecho/index.php">Home</a></li>
+              <li><a href="/guia_brecho/views/produtos.php">Produtos</a></li>
+              <li><a href="/guia_brecho/views/loja.php">Lojas</a></li>
+              <li><a href="/guia_brecho/views/quemsomos.php">Quem Somos?</a></li>
+                <li>
+                    <a href="/guia_brecho/views/login.php"><span class="material-symbols-outlined">
+                        person
+                        </span>Login</a>
+                </li>
+            </ul>
+        </nav>        
     </header>
 
     <main>
