5f1f5942cd09baf66739cbfafb22badeef61a4c4
HandrewEspectacao <handrewes.99@gmail.com>
2023-06-20 16:26:15 -0300

    marcações sendo feitas

--- views/eventos.php
+++ views/eventos.php
@@ -55,6 +55,7 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+<!-- fim do carrossel -->
 
 
 <!-- Marketing messaging and featurettes
@@ -63,14 +64,14 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
 
 <div class="container marketing">
 
-  <!-- Three columns of text below the carousel -->
+   <!--Three columns of text below the carousel 
   <div class="row">
     <div class="col-lg-4">
-      <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
+      <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="/guia_brecho/img/img.png" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
       <h2 class="fw-normal">Heading</h2>
       <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
       <p><a class="btn btn-secondary" href="#">View details »</a></p>
-    </div><!-- /.col-lg-4 -->
+    </div>
     <div class="col-lg-4">
       <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
       <h2 class="fw-normal">Heading</h2>
@@ -136,7 +137,6 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
   <p class="float-end"><a href="#">Back to top</a></p>
   <p>© 2017–2023 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
 </footer>
-</main>
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
 ?>
