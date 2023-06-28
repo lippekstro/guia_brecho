52795dc732e160efab53a5a59ec081fbf144db02
lippekstro <fellipecastro13@hotmail.com>
2023-06-28 19:32:50 -0300

    imagem de eventos

--- db/banco.sql
+++ db/banco.sql
@@ -7,7 +7,7 @@ CREATE TABLE usuario (
     email VARCHAR(255) NOT NULL,
     cpf_cnpj BIGINT UNIQUE NOT NULL, 
     nivel_acesso int DEFAULT 1
-)
+);
 
 CREATE TABLE loja (
     id_loja int PRIMARY KEY AUTO_INCREMENT, 
@@ -18,7 +18,7 @@ CREATE TABLE loja (
     logo_loja LONGBLOB,
     id_usuario int NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
-)
+);
 
 CREATE TABLE produto (
     id_produto int PRIMARY KEY AUTO_INCREMENT, 
@@ -30,13 +30,14 @@ CREATE TABLE produto (
     imagem_produto LONGBLOB, 
     id_loja int NOT NULL,
     FOREIGN KEY (id_loja) REFERENCES loja(id_loja)
-)
+);
 
 CREATE TABLE evento (
     id_evento int PRIMARY KEY AUTO_INCREMENT,  
     data_evento DATE NOT NULL, 
-    horario TIME NOT NULL, 
+    horario TIME NOT NULL,
+    imagem_evento LONGBLOB, 
     local_evento VARCHAR(255) NOT NULL,
     descricao_evento TEXT NOT NULL
-)
+);
 
