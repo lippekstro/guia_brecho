3a662f99d09060d618a1612644d174159f215326
darisson <>
2023-06-28 20:49:31 -0300

    modificação do home e footer

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -1,18 +1,57 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Guia Brechó</title>
-    <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
+    <link rel="stylesheet" href="/guia_brecho/css/stylehome.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+    integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="home.js" defer></script>
+
+  <title>Guia Brechó</title>
 </head>
+
 <body>
-    <header>
-        <section>
-            
-        </section>
+  <!-- header -->
+  <header style="background-image: url(/guia_brecho/img/back02.png);">
+    <!-- conteiner -->
+    <div class="conteiner">
+      <div class="menu_fixo" id="menu_fixo">
 
-    </header>
+      <nav class="menu_nav">
+        <a href="home.html"> 
+          <div class="img_logo">
+          <img src="img/sem fundo.png" alt="guia brechó" >
+        </div>
+          <ul>
+            <a href="home.html">Home</a>
+            <a href="home.html">Produtos</a>
+            <a href="home.html">Lojas</a>
+            <a href="home.html">Eventos</a>
+            <a href="home.html">Quem somos?</a>
+            <a class="btn" href="home.html"><img src="img/login branco.png" alt="" width="30px"></a> 
 
-    <main>
+          </ul>
+        </a>
+      </nav>
+    </div>
+
+      <section class="banner">
+        <div class="banner-text">
+          <h1> GUIA BRECHÓ</h1>
+          <p>o melhor site brechó de São luís</p>
+          <!--  <a href="home.html" class="btn">Login</a>
+ -->
+        </div>
+      </section>
+    </div>
+    <!-- fim conteiner -->
+  </header>
+  <!-- fim header -->
+</body>
+</html>
---
+++ templates/rodape.php
@@ -0,0 +1,189 @@
+ 
+ 
+ 
+ 
+ <!-- inicio do footer  -->
+
+ <link rel="stylesheet" href="stylehome.css">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+  <footer id="dk-footer" class="dk-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 col-lg-4">
+          <div class="dk-footer-box-info">
+            <a href="index.html" class="footer-logo">
+              <img src="img/sem fundo.png " width="180px" alt="footer_logo" class="img-fluid">
+            </a>
+            <p class="footer-info-text">
+              Conheça nossas redes sociais. 
+            </p>
+            <div class="footer-social-link">
+              <h3>Siga-nos</h3>
+              <ul>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-google-plus"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="fa fa-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <!-- End Social link -->
+          </div>
+          <!-- End Footer info -->
+        
+        </div>
+        <!-- End Col -->
+        <div class="col-md-12 col-lg-8">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="contact-us">
+                <div class="contact-icon">
+                  <i class="fa fa-map-o" aria-hidden="true"></i>
+                </div>
+                <!-- End contact Icon -->
+                <div class="contact-info">
+                  <h3>Jaipur India</h3>
+                  <p>5353 Road Avenue</p>
+                </div>
+                <!-- End Contact Info -->
+              </div>
+              <!-- End Contact Us -->
+            </div>
+            <!-- End Col -->
+            <div class="col-md-6">
+              <div class="contact-us contact-us-last">
+                <div class="contact-icon">
+                  <i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+                </div>
+                <!-- End contact Icon -->
+                <div class="contact-info">
+                  <h3>95 711 9 5353</h3>
+                  <p>Give us a call</p>
+                </div>
+                <!-- End Contact Info -->
+              </div>
+              <!-- End Contact Us -->
+            </div>
+            <!-- End Col -->
+          </div>
+          <!-- End Contact Row -->
+          <div class="row">
+            <div class="col-md-12 col-lg-6">
+              <div class="footer-widget footer-left-widget">
+                <div class="section-heading">
+                  <h3>Links</h3>
+                  <span class="animate-border border-black"></span>
+                </div>
+                <ul>
+                  <li>
+                    <a href="#">produtos</a>
+                  </li>
+                  <li>
+                    <a href="#">lojas</a>
+                  </li>
+                  <li>
+                    <a href="#">eventos</a>
+                  </li>
+                  <li>
+                    <a href="#">quem somos?</a>
+                  </li>
+                </ul>
+                <ul>
+                  <li>
+                    <a href="#">login</a>
+                  </li>
+                </ul>
+              </div>
+              <!-- End Footer Widget -->
+            </div>
+            <!-- End col -->
+            <div class="col-md-12 col-lg-6">
+              <div class="footer-widget">
+                <div class="section-heading">
+                  <h3>Subscribe</h3>
+                  <span class="animate-border border-black"></span>
+                </div>
+                <p><!-- Don’t miss to subscribe to our new feeds, kindly fill the form below. -->
+                  Reference site about Lorem Ipsum, giving information on its origins, as well.</p>
+                <form action="#">
+                  <div class="form-row">
+                    <div class="col dk-footer-form">
+                      <input type="email" class="form-control" placeholder="Email">
+                      <button type="submit">
+                        <i class="fa fa-send"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+                <!-- End form -->
+              </div>
+              <!-- End footer widget -->
+            </div>
+            <!-- End Col -->
+          </div>
+          <!-- End Row -->
+        </div>
+        <!-- End Col -->
+      </div>
+      <!-- End Widget Row -->
+    </div>
+    <!-- End Contact Container -->
+
+
+    <div class="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <span>Copyright © 2023, reservado por Guia Brechó</span>
+          </div>
+          <!-- End Col -->
+          <div class="col-md-6">
+            <div class="copyright-menu">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- End col -->
+        </div>
+        <!-- End Row -->
+      </div>
+      <!-- End Copyright Container -->
+    </div>
+    <!-- End Copyright -->
+    <!-- Back to top -->
+    <div id="back-to-top" class="back-to-top">
+      <button class="btn btn-dark" title="Back to Top" style="display: block;">
+        <i class="fa fa-angle-up"></i>
+      </button>
+    </div>
+    <!-- End Back to top -->
+  </footer>
+
+
+  <!-- FIM DO FOOTER -->
