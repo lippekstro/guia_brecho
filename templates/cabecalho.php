1fa456ecf510ad279876e0d79081a898f07bde65
Gordadomar <juliana.marques.coelho@gmail.com>
2023-06-28 19:50:23 -0300

    mudanca de caminho

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -6,22 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guia Brechó</title>
     
-    <link rel="stylesheet" href="/PHPORRA/guia_brecho/css/swiper-bundle.min.css">
-    <link rel="shortcut icon" href="/PHPORRA/guia_brecho/img/img.png" type="image/x-icon">
+    <link rel="stylesheet" href="/guia_brecho/css/swiper-bundle.min.css">
+    <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="/PHPORRA/guia_brecho/css/style.css">
+    <link rel="stylesheet" href="/guia_brecho/css/style.css">
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li> <img src="/PHPORRA/guia_brecho/img/logo_guia_brecho-rem3.png" class="logowidth"> </li>
-                <li><a href="/PHPORRA/guia_brecho/index.php">Home</a></li>
-                <li><a href="/PHPORRA/guia_brecho/views/produtos.php">Produtos</a></li>
-                <li><a href="/PHPORRA/guia_brecho/views/loja.php">Lojas</a></li>
-                <li><a href="/PHPORRA/guia_brecho/views/quemsomos.php">Quem Somos?</a></li>
+                <li> <img src="/guia_brecho/img/logo_guia_brecho-rem3.png" class="logowidth"> </li>
+                <li><a href="/guia_brecho/index.php">Home</a></li>
+                <li><a href="/guia_brecho/views/produtos.php">Produtos</a></li>
+                <li><a href="/guia_brecho/views/loja.php">Lojas</a></li>
+                <li><a href="/guia_brecho/views/quemsomos.php">Quem Somos?</a></li>
                 <li>
-                    <a href="/PHPORRA/guia_brecho/views/login.php"><span class="material-symbols-outlined">
+                    <a href="/guia_brecho/views/login.php"><span class="material-symbols-outlined">
                         person
                         </span>Login</a>
                 </li>
