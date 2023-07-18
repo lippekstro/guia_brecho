03c5b7ddecf6d40c8d787736c92c88db636cb5a9
Dnmaster-Dev <estevaothatymartins2325@gmail.com>
2023-07-18 19:57:28 -0300

    Removi rodapé

--- templates/rodape.php
+++ templates/rodape.php
@@ -2,22 +2,7 @@
 
 
 <footer>
-    <div class="left">
-        <a href="url_do_seu_link" class="link-sem-barra">
-            <img src="/guia_brecho/img/facebook.png" alt="Facebook" class="imgfooter">
-        </a> 
-        <a href="url_do_seu_link" class="link-sem-barra">
-            <img src="/guia_brecho/img/instagram.png" alt="Instagram" class="imgfooter">
-        </a>  
-        <a href="url_do_seu_link" class="link-sem-barra">
-            <img src="/guia_brecho/img/twitter.png" alt="Twitter" class="imgfooter">
-        </a>       
-    </div>
-    <div class="right">
-      <p>Guia Brecho | CNPJ: 05754869/0001- 45 - I.E. 78.289.821</p>
-      <p>Rua do Giz - São Luís - CEP: 22221-070</p>
-      <p>contato@guiabrechoslz.com.br</p>
-    </div>
+   
 </footer>
   
 </body>
