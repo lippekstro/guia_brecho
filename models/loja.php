c70c86295cdf73f26dd095fae81f7e70f13bfdfe
Gordadomar <juliana.marques.coelho@gmail.com>
2023-07-19 20:52:00 -0300

    descrição dos brechos

---
+++ models/loja.php
@@ -0,0 +1,215 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
+?>
+    <div class="slide-container swiper">
+            <div class="slide-content">
+                <div class="card-wrapper swiper-wrapper">
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile1.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechaos</h2>
+                            <p class="description"> 
+                                @brechaos |  Aqui você encontra peças novas e usadas garimpadas com amor 🤍
+                                • Enviamos para todo Brasil ✈️
+                                • Sem trocas e devolução
+                                • Compre via link ou direct ⬇️
+                                linktr.ee/brechaos1</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile2.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            
+                            <h2 class="name">Itens de Brechó</h2>
+                            <p class="description">
+                            @itensdebrecho |
+                            Roupas e vestuário
+                            ✿ Toda peça de brechó é uma oportunidade única!
+                            #itemdisp
+                            ✿ Atualização nos Storys ✨
+                            ✿ não fazemos trocas|devolução|cancelamentos </p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile3.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechó Girl Boss</h2>
+                            <p class="description"> 
+                                @bregirlboss |
+                                Há 3 anos na moda sustentável!
+                                • Acompanhe os stories ✨
+                                • Entregas facilitadas- 1x na semana
+                                • Apoie uma mulher empreendedora 
+                                • São Luís, MARANHÃO.</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile4.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Mais Um Brechó</h2>
+                            <p class="description"> 
+                                @maisumbrecho |
+                                Roupas e vestuário
+                                Desde 2017
+                                Do Maranhão para todo Brasil
+                                Para mais informações, leia o nosso destaque
+                                Sem trocas/devolução
+                                #maisumdispo</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile5.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechó Caramujo</h2>
+                            <p class="description">@brechocaramujo |
+                                Vestuário 
+                                Comércio Virtual em São Luís MA
+                                • For Men - 2020
+                                ❌ Não fazemos reservas (apenas por transferência)
+                                ❌ Não fazemos trocas
+                                wa.me/5598987140402.</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile6.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechó Estrela</h2>
+                            <p class="description"> 
+                                @brechoestrela | Brechó Online
+                                • Vintage/Moderno/Sexy
+                                • Você é uma estrela que nasceu pra brilhar! ✨
+                                • Enviamos pra todo BR | De São Luís.
+                                #brechoesteladispo</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile7.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechó Art</h2>
+                            <p class="description">@brecho&art |
+                                Produto/serviço
+                                FAZENDO DO BRECHÓ UMA ARTE!
+                                ☻ vendas pelo direct
+                                ☻ facilitamos sua entrega
+                                ☻ são luís, ma.
+                                Sem devolução e trocas
+                                #brechoartdispo</p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile8.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Agá Brechó</h2>
+                            <p class="description">@agabrecho | Agá Brechó
+                                Roupa e Vestuário
+                                Por: @mlyvale
+                                Aceitamos somente transferências ou pix.
+                                Sem trocas sem devolução X
+                                São Luís-MA.
+                                #agabrechodisp
+                                </p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                    <div class="card swiper-slide">
+                        <div class="image-content">
+                            <span class="overlay"></span>
+
+                            <div class="card-image">
+                                <img src="/guia_brecho/img/profile9.png" alt="" class="card-img">
+                            </div>
+                        </div>
+
+                        <div class="card-content">
+                            <h2 class="name">Brechó Garimpeira</h2>
+                            <p class="description">@garimpeira_brech | Garimpeira Brechó Slz
+                                ✨Vintage moderno atemporal
+                                ✨NÃO COMPRAMOS DESAPEGO!
+                                ✨Desde 2020
+                                🚚 entrega moto boy /terminal cohab sabados a tarde
+                                São Luís - MA
+                                </p>
+
+                            <button class="button">Saiba Mais</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="swiper-button-next swiper-navBtn"></div>
+            <div class="swiper-button-prev swiper-navBtn"></div>
+            <div class="swiper-pagination"></div>
+        </div>
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
+?>
