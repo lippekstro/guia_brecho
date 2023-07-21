622cff45aad7fab0a3906596dfbfeb1954c2b55e
ribeiro-fabricio <fabriciozrs@gmail.com>
2023-07-21 20:40:11 -0300

    Ajustes de cadastrar produto

--- js/cadastroBrecho.js
+++ js/cadastroBrecho.js
@@ -23,9 +23,65 @@ document.addEventListener('click', function (event) {
 );
 
 
+
+document.addEventListener('click', function (event) {
+  // Verifica se o elemento clicado é do tipo input
+
+  if (event.target.tagName.toLowerCase() === 'input')
+     {
+      var inputId = event.target.id;
+      //console.log(inputId) precisa de implementação para ser somente no cadastro_brecho
+      var Nomeinput = document.getElementById(inputId);
+      console.log(Nomeinput);
+      console.log("aquiuii");
+      console.log(inputId);
+      // Obtém o ID do input clicado
+      if((inputId == "files")||(inputId == "imagem_produto")){
+        console.log("entrouuuuu");
+      
+        //Implementação da Foto de LOGO
+        var files = Nomeinput;//document.querySelector('input[name="inputId"]');
+        console.log('aqui abaixo');
+        console.log(files);
+
+        files.addEventListener("change", function (file) {
+          var input = file.target;
+
+          var reader = new FileReader();
+
+          reader.onload = function () {
+            var dataURL = reader.result;
+            if(inputId == "imagem_produto"){
+              var outputCadProduto = document.getElementById('outputCadProduto');
+              outputCadProduto.src = dataURL;
+            }
+            var output = document.getElementById('output');
+            output.src = dataURL;
+          };
+
+          reader.readAsDataURL(input.files[0]);
+        }
+      );
+
+
+
+
+    }
+
+  }
+}
+);
+
+
+
+
+
+//}
+
+/*
 //Implementação da Foto de LOGO
 var files = document.querySelector('input[name="files"]');
-
+console.log(files);
 files.addEventListener("change", function (file) {
   var input = file.target;
 
@@ -39,3 +95,4 @@ files.addEventListener("change", function (file) {
 
   reader.readAsDataURL(input.files[0]);
 });
+*/
