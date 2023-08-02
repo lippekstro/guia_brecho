c712b625a6cefe0e5783e9097346bb82e2b4347f
Yribeiro-fabricio <fabriciozrs@gmail.com>
2023-08-02 20:24:58 -0300

    atualização do controller cadastro brecho

--- controllers/cadastro_brecho_controller.php
+++ controllers/cadastro_brecho_controller.php
@@ -4,19 +4,48 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "\guia_brecho\models\brecho.php";
 
 
-if (isset($_POST['nomebrecho']) && isset($_POST['cnpj-cpf']) && isset($_POST['cidade'])
-     && isset($_POST['endereco']) && isset($_POST['numero']) && isset($_POST['bairro'])
-     && isset($_FILES['files']) && isset($_POST['contato']) && isset($_POST['pagamento']) 
-     && isset($_POST['faixa-preco']) && isset($_POST['hora']) && isset($_POST['entrega'])
-     && isset($_POST['story']) && isset($_POST['termo']) 
+if  //Dados Pessoas do Dono do Brecho:
+    //Dados Pessoais
+    isset($_POST['nome']) && isset($_POST['sobrenome']) && isset($_POST['email']) && isset($_POST['password']) &&
 
-     ) {
-    $nomebrecho = $_POST['nomebrecho'];
-    $cnpj = $_POST['cnpj-cpf'];
-    $cidade = $_POST['cidade'];
+    //Dados do Brecho:
+    (isset($_POST['nomebrecho']) && isset($_POST['cnpj-cpf']) && isset($_POST['cidade'])
+     && isset($_POST['endereco']) && isset($_POST['numero']) && isset($_POST['bairro']) && 
+     
+     //Mídias do Brecho
+     isset($_FILES['files']) && isset($_POST['redeSocial']) && isset($_POST['contato']) &&
+     isset($_POST['redeSocial']) &&  isset($_POST['redeSocial']) && isset($_POST['contato']) &&
+     redeSocial
+     
 
+     //CONTINUE SEU CADASTRO
+     //Informações para Divulgações
+     isset($_POST['paga_pix']) && isset($_POST['paga_ted']) && isset($_POST['paga_boleto']) && isset($_POST['paga_dinheiro']) 
+     && isset($_POST['faixa_preco_ini']) && isset($_POST['faixa_preco_fim']) && isset($_POST['hora']) && isset($_POST['entrega'])
+     && isset($_POST['story']) && isset($_POST['termo'])) 
+     
+    {
+    //Dados Pessoais
+    $pessoa_nome = $_POST['nome'];
+    $pessoa_sobrenome= $_POST['sobrenome'];
+    $pessoa_email = $_POST['email'];
+    $pessoa_password = $_POST['password'];
+    
+    //Dados do Brecho:
+    $brecho_nome = $_POST['nomebrecho'];
+    $brecho_cnpj = $_POST['cnpj-cpf'];
+    $brecho_cidade = $_POST['cidade'];
+    $brecho_endereco = $_POST['endereco'];
+    $brecho_numero = $_POST['numero'];
+    $brecho_bairro = $_POST['bairro'];
+    
+    //Mídias do Brecho    
+    $brecho_img = $_FILES['files']['name'];
+    $brecho_rede= $_POST['redeSocial'];
+    $brecho_contato= $_POST['contato'];
 
-    $img_brecho = $_FILES['files']['name'];
+    //Informações para Divulgações   
+    $brecho_paga_pix $_POST['paga_pix'];
 
 
 
