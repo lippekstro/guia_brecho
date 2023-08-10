260ec0acdf097eb077a27fccf09fcb13658675bb
ribeiro-fabricio <fabriciozrs@gmail.com>
2023-08-10 16:56:28 -0300

    Inserir_conrtroller_brecho

--- controllers/cadastro_brecho_controller.php
+++ controllers/cadastro_brecho_controller.php
@@ -4,28 +4,32 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "\guia_brecho\models\brecho.php";
 
 
-if  //Dados Pessoas do Dono do Brecho:
-    //Dados Pessoais
-    isset($_POST['nome']) && isset($_POST['sobrenome']) && isset($_POST['email']) && isset($_POST['password']) &&
+//Dados Pessoas do Dono do Brecho:
+//Dados Pessoais
+if(isset($_POST['nome']) && isset($_POST['sobrenome'])
+&& isset($_POST['email']) && isset($_POST['password'])
+&&
 
-    //Dados do Brecho:
-    (isset($_POST['nomebrecho']) && isset($_POST['cnpj-cpf']) && isset($_POST['cidade'])
-     && isset($_POST['endereco']) && isset($_POST['numero']) && isset($_POST['bairro']) && 
+//Dados do Brecho:
+isset($_POST['nomebrecho']) && isset($_POST['cnpj-cpf'])
+&& isset($_POST['cidade']) && isset($_POST['endereco'])
+&& isset($_POST['numero']) && isset($_POST['bairro'])
+&& 
      
-     //Mídias do Brecho
-     isset($_FILES['files']) && isset($_POST['redeSocial']) && isset($_POST['contato']) &&
-     isset($_POST['redeSocial']) &&  isset($_POST['redeSocial']) && isset($_POST['contato']) &&
-     redeSocial
+//Mídias do Brecho
+isset($_FILES['files']) && isset($_POST['redeSocial'])
+&& isset($_POST['contato'])
+&&
      
-
-     //CONTINUE SEU CADASTRO
-     //Informações para Divulgações
-     isset($_POST['paga_pix']) && isset($_POST['paga_ted']) && isset($_POST['paga_boleto']) && isset($_POST['paga_dinheiro']) 
-     && isset($_POST['faixa_preco_ini']) && isset($_POST['faixa_preco_fim']) && isset($_POST['hora']) && isset($_POST['entrega'])
-     && isset($_POST['story']) && isset($_POST['termo'])) 
-     
-    {
-    //Dados Pessoais
+//CONTINUE SEU CADASTRO
+//Informações para Divulgações
+isset($_POST['paga_pix']) && isset($_POST['paga_ted'])
+&& isset($_POST['paga_boleto']) && isset($_POST['paga_dinheiro']) 
+&& isset($_POST['faixa_preco_ini']) && isset($_POST['faixa_preco_fim'])
+&& isset($_POST['hora']) && isset($_POST['entrega'])
+&& isset($_POST['story']) && isset($_POST['termo'])
+)
+{
     $pessoa_nome = $_POST['nome'];
     $pessoa_sobrenome= $_POST['sobrenome'];
     $pessoa_email = $_POST['email'];
@@ -65,10 +69,12 @@ if  //Dados Pessoas do Dono do Brecho:
     $brecho_faixa_preco_ini  = floatval($brecho_faixa_preco_ini); 
     $brecho_faixa_preco_fim  = floatval($brecho_faixa_preco_fim); 
 
-  
+
     $brecho = new Brecho();
     $brecho->cadastrarBrecho($pessoa_nome, $pessoa_sobrenome, $pessoa_email, $pessoa_password,$brecho_nome,$brecho_cnpj,$brecho_cidade,$brecho_endereco,$brecho_numero,$brecho_bairro,$brecho_img,
     $brecho_rede, $brecho_contato, $brecho_paga_pix, $brecho_paga_ted,$brecho_paga_boleto,$brecho_paga_dinheiro, $brecho_faixa_preco_ini, $brecho_faixa_preco_fim,$brecho_hora,$brecho_entrega,
     $brecho_story,$brecho_termo);
+    
 }
+
 
--- models/brecho.php
+++ models/brecho.php
@@ -1,92 +1,109 @@
-<?php
-require_once $_SERVER["DOCUMENT_ROOT"] . '\guia_brecho\db\conexao.php';
+    <?php
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/db/conexao.php";
 
-class Brecho {
-    public $pessoa_nome;
-    public $pessoa_sobrenome;
-    public $pessoa_email;
-    public $pessoa_password;
-    
-    public $brecho_nome;
-    public $brecho_cnpj;
-    public $brecho_cidade;
-    public $brecho_endereco;
-    public $brecho_numero;
-    public $brecho_bairro;
-
-    public $brecho_img;
-    public $brecho_rede;
-    public $brecho_contato;
-
-    public $brecho_paga_pix;
-    public $brecho_paga_ted;
-    public $brecho_paga_boleto;
-    public $brecho_paga_dinheiro;
-
-    public $brecho_faixa_preco_ini;
-    public $brecho_faixa_preco_fim;
-
-    public $brecho_hora;
-    public $brecho_entrega;
-
-    public $brecho_story;
-    public $brecho_termo;
-
-
-    //INSERIR NO BANCO OS DADOS DO BRECHÓ
-
-    public function cadastrarBrecho($pessoa_nome, $pessoa_sobrenome, $pessoa_email, $pessoa_password,$brecho_nome,$brecho_cnpj,$brecho_cidade,$brecho_endereco,$brecho_numero,$brecho_bairro,$brecho_img,
-    $brecho_rede, $brecho_contato, $brecho_paga_pix, $brecho_paga_ted,$brecho_paga_boleto,$brecho_paga_dinheiro, $brecho_faixa_preco_ini, $brecho_faixa_preco_fim,$brecho_hora,$brecho_entrega,
-    $brecho_story,$brecho_termo)
-    {
-        $query = "INSERT INTO produto (pessoa_nome, pessoa_sobrenome, pessoa_email, pessoa_password,....)
-                VALUES (:pessoa_nome, :pessoa_sobrenome, :pessoa_email, :pessoa_password)";
-
-        $conexao = Conexao::conectar();
-        $stmt = $conexao->prepare($query);
-        $stm ->bindValue(':pessoa_nome', $pessoa_nome );
-        $stm ->bindValue(':pessoa_sobrenome', $pessoa_sobrenome );
-        $stm ->bindValue(':pessoa_email', $pessoa_email );
-        $stm ->bindValue(':pessoa_password', $pessoa_password );
-
-        $stm ->bindValue(':brecho_nome', $brecho_nome );
-        $stm ->bindValue(':brecho_cnpj', $brecho_cnpj );
-        $stm ->bindValue(':brecho_cidade', $brecho_cidade );
-        $stm ->bindValue(':brecho_endereco', $brecho_endereco );
-        $stm ->bindValue(':brecho_numero', $brecho_numero );
-        $stm ->bindValue(':brecho_bairro', $brecho_bairro );
-
-        $stm ->bindValue(':brecho_img', $brecho_img );
-        $stm ->bindValue(':brecho_rede', $brecho_rede );
-        $stm ->bindValue(':brecho_contato', $brecho_contato );
-
-        $stm ->bindValue(':brecho_paga_pix', $brecho_paga_pix );
-        $stm ->bindValue(':brecho_paga_ted', $brecho_paga_ted );
-        $stm ->bindValue(':brecho_paga_boleto', $brecho_paga_boleto );
-        $stm ->bindValue(':brecho_paga_dinheiro', $brecho_paga_dinheiro );
-
-        $stm ->bindValue(':brecho_faixa_preco_ini', $brecho_faixa_preco_ini );
-        $stm ->bindValue(':brecho_faixa_preco_fim', $brecho_faixa_preco_fim );
-
-        $stm ->bindValue(':brecho_hora', $brecho_hora );
-        $stm ->bindValue(':brecho_entrega', $brecho_entrega );
-
-        $stm ->bindValue(':brecho_story', $brecho_story);
-        $stm ->bindValue(':brecho_termo', $brecho_termo);
+    class Brecho {
+        public $pessoa_nome;
+        public $pessoa_sobrenome;
+        public $pessoa_email;
+        public $pessoa_password;
         
-        if (!empty($brecho_img)) {
-            $brecho_img_tmp = $_FILES['brecho_img']['tmp_name'];
-            $brecho_img_contents = file_get_contents($img_produto_tmp);
-            $stmt->bindValue(':brecho_img', $img_produto_contents, PDO::PARAM_LOB);
-        } else {
-            $stmt->bindValue(':brecho_img', null, PDO::PARAM_NULL);
+        public $brecho_nome;
+        public $brecho_cnpj;
+        public $brecho_cidade;
+        public $brecho_endereco;
+        public $brecho_numero;
+        public $brecho_bairro;
+
+        public $brecho_img;
+        public $brecho_rede;
+        public $brecho_contato;
+
+        public $brecho_paga_pix;
+        public $brecho_paga_ted;
+        public $brecho_paga_boleto;
+        public $brecho_paga_dinheiro;
+
+        public $brecho_faixa_preco_ini;
+        public $brecho_faixa_preco_fim;
+
+        public $brecho_hora;
+        public $brecho_entrega;
+
+        public $brecho_story;
+        public $brecho_termo;
+    
+
+        //INSERIR NO BANCO OS DADOS DO BRECHÓ
+
+        public function cadastrarBrecho($pessoa_nome, $pessoa_sobrenome, $pessoa_email, $pessoa_password,$brecho_nome,$brecho_cnpj,$brecho_cidade,$brecho_endereco,$brecho_numero,$brecho_bairro,$brecho_img,
+        $brecho_rede, $brecho_contato, $brecho_paga_pix, $brecho_paga_ted,$brecho_paga_boleto,$brecho_paga_dinheiro, $brecho_faixa_preco_ini, $brecho_faixa_preco_fim,$brecho_hora,$brecho_entrega,
+        $brecho_story,$brecho_termo)
+        {    echo"preparou comando ?";
+            $query_usuario = "INSERT INTO usuario (nome, senha, email, cpf_cnpj)
+                    VALUES (:pessoa_nome, :pessoa_password, :pessoa_email, :brecho_cnpj)";
+
+            $query_loja = "INSERT INTO loja (nome_loja, endereco, telefone, rede_social, logo_loja)
+            VALUES (:brecho_nome, :brecho_endereco, :brecho_contato, :brecho_rede, :brecho_img)";
+
+            $conexao = Conexao::conectar();
+            
+            $stmt_usuario = $conexao->prepare($query_usuario);
+
+            $stmt_loja = $conexao->prepare($query_loja);
+
+
+            $stmt_usuario ->bindValue(':pessoa_nome', $pessoa_nome );
+            $stmt_usuario ->bindValue(':pessoa_sobrenome', $pessoa_sobrenome );
+            $stmt_usuario ->bindValue(':pessoa_email', $pessoa_email );
+            $stmt_usuario ->bindValue(':pessoa_password', $pessoa_password );
+
+            $stmt_loja ->bindValue(':brecho_nome', $brecho_nome );
+            $stmt_loja ->bindValue(':brecho_cnpj', $brecho_cnpj );
+            $stmt_loja ->bindValue(':brecho_cidade', $brecho_cidade );
+            $stmt_loja ->bindValue(':brecho_endereco', $brecho_endereco );
+            $stmt_loja ->bindValue(':brecho_numero', $brecho_numero );
+            $stmt_loja ->bindValue(':brecho_bairro', $brecho_bairro );
+
+            $stmt_loja ->bindValue(':brecho_img', $brecho_img );
+            $stmt_loja ->bindValue(':brecho_rede', $brecho_rede );
+            $stmt_loja ->bindValue(':brecho_contato', $brecho_contato );
+
+            $stmt_loja ->bindValue(':brecho_paga_pix', $brecho_paga_pix );
+            $stmt_loja ->bindValue(':brecho_paga_ted', $brecho_paga_ted );
+            $stmt_loja ->bindValue(':brecho_paga_boleto', $brecho_paga_boleto );
+            $stmt_loja ->bindValue(':brecho_paga_dinheiro', $brecho_paga_dinheiro );
+
+            $stmt_loja ->bindValue(':brecho_faixa_preco_ini', $brecho_faixa_preco_ini );
+            $stmt_loja ->bindValue(':brecho_faixa_preco_fim', $brecho_faixa_preco_fim );
+
+            $stmt_loja ->bindValue(':brecho_hora', $brecho_hora );
+            $stmt_loja ->bindValue(':brecho_entrega', $brecho_entrega );
+
+            $stmt_loja ->bindValue(':brecho_story', $brecho_story);
+            $stmt_loja ->bindValue(':brecho_termo', $brecho_termo);
+            
+            if (!empty($brecho_img)) {
+                $brecho_img_tmp = $_FILES['brecho_img']['tmp_name'];
+                $brecho_img_contents = file_get_contents($brecho_img_tmp);
+                $stmt_loja->bindValue(':brecho_img', $brecho_img_contents, PDO::PARAM_LOB);
+            } else {
+                $stmt_loja->bindValue(':brecho_img', null, PDO::PARAM_NULL);
+            }
+
+            try {
+                $stmt_usuario->execute();
+            } catch (PDOException $e)
+                {echo "Erro na para a tabela de usuario: " . $e->getMessage();}
+            
+            try {
+                $stmt_loja->execute();
+            } catch (PDOException $e) 
+                {echo "Erro na para a tabela da loja: " . $e->getMessage();}
+
+            echo "chegou aqui ??";
+            header("Location: \guia_brecho\index.php"); //redirecionando para a pagina principal
         }
 
-        $stmt->execute();
-        header("Location: \guia_brecho\index.php"); //redirecionando para a pagina principal
+
+
     }
-
-
-
-
-}
