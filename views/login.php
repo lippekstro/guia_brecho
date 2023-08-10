a58118631c4379615dcf006c7ca089092a322c3c
ribeiro-fabricio <fabriciozrs@gmail.com>
2023-08-10 18:09:20 -0300

    Controller Login

--- views/login.php
+++ views/login.php
@@ -1,22 +1,24 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
+
 ?>
 
 <div>
-    <form action="/guia_brecho/controllers/login_controller.php" autocomplete="on" method="POST">
-        <fieldset id="loginmod">
-            <h1 id="h1style">Login</h1>
-            <p class="fontelogin" style="margin-bottom: 10px;">Digite os seus dados de acesso nos campos abaixo.</p>
-            <div style="margin-bottom: 10px;">
-                <input type="mail" class="bordainput meu-input" name="email" id="email" placeholder="E-mail" required autofocus>
-            </div>
-            <div style="margin-bottom: 10px;">
-                <input type="password" class="bordainput meu-input" name="senha" id="senha" placeholder="Senha" required>
-            </div>
-            <button type="submit" id="buttonb">Acessar</button>
-            <p style="margin-top: 5px;"><a href="/guia_brecho/views/cadastro_brecho.php" id="linkb">Cadastrar</a></p>
-        </fieldset>
-    </form>
+        <form action="/guia_brecho/controllers/login_controller.php" autocomplete="on" method="POST">
+            <fieldset id="loginmod">
+                <h1 id="h1style">Login</h1>
+                <p class= "fontelogin" style="margin-bottom: 10px;">Digite os seus dados de acesso nos campos abaixo.</p> 
+                <div style="margin-bottom: 10px;">
+                    <input type="mail" class="bordainput meu-input" name="email" id="email" placeholder="E-mail" required autofocus>
+                </div>
+                <div style="margin-bottom: 10px;">
+                    <input type="password" class="bordainput meu-input" name="senha" id="senha" placeholder="Senha" required>
+                </div>
+                <button type="submit" id="buttonb">Acessar</button>
+                <p  style="margin-top: 5px;"><a href="/guia_brecho/views/cadastro_usuario.php"  id="linkb">Cadastrar</a></p> 
+
+            </fieldset>
+         </form>
 </div>
 
 <?php
