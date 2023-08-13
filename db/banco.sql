42f2fbe4efda811f8dc5898fd010ff5b31e265e5
ribeiro-fabricio <fabriciozrs@gmail.com>
2023-08-13 00:39:16 -0300

    sanitização

--- db/banco.sql
+++ db/banco.sql
@@ -2,7 +2,8 @@ CREATE DATABASE guiabrecho;
 
 CREATE TABLE usuario (
     id_usuario int PRIMARY KEY AUTO_INCREMENT, 
-    nome VARCHAR(255) NOT NULL, 
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL, 
     email VARCHAR(255) NOT NULL,
     cpf_cnpj BIGINT UNIQUE NOT NULL, 
@@ -12,10 +13,23 @@ CREATE TABLE usuario (
 CREATE TABLE loja (
     id_loja int PRIMARY KEY AUTO_INCREMENT, 
     nome_loja VARCHAR(255) NOT NULL, 
-    endereco VARCHAR(255) NOT NULL, 
+    cidade VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL,
+    bairro VARCHAR(255) NOT NULL,
     telefone BIGINT NOT NULL, 
     rede_social VARCHAR(255) NOT NULL,
     logo_loja LONGBLOB,
+    paga_pix VARCHAR(255),
+    paga_ted VARCHAR(255) ,
+    paga_boleto VARCHAR(255),
+    paga_dinheiro VARCHAR(255),
+    faixa_preco_ini VARCHAR(255),
+    faixa_preco_fim VARCHAR(255),
+    hora VARCHAR(255),
+    entrega VARCHAR(255),
+    story VARCHAR(255),
+    termo VARCHAR(255),
     id_usuario int NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
