b958ab9e7802e5c92aa85edf0b9090394cb743be
Felippe Fialho <felippefialho@hotmail.com>
2023-08-14 17:15:29 -0300

    responsividade da pag produtos

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -26,6 +26,8 @@
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
+    <script src="/guia_brecho/js/script-produtos.js" defer></script>
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:400,300'>
