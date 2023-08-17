b68c9493f7dfc76c41184c5b8ac91335a3d1d8ec
lippekstro <fellipecastro13@hotmail.com>
2023-08-17 19:36:57 -0300

    ajustes no model brecho

--- db/banco.sql
+++ db/banco.sql
@@ -10,27 +10,17 @@ CREATE TABLE usuario (
     nivel_acesso int DEFAULT 1
 );
 
-CREATE TABLE loja (
-    id_loja int PRIMARY KEY AUTO_INCREMENT, 
-    nome_loja VARCHAR(255) NOT NULL, 
-    cidade VARCHAR(255) NOT NULL,
-    endereco VARCHAR(255) NOT NULL,
-    numero VARCHAR(255) NOT NULL,
-    bairro VARCHAR(255) NOT NULL,
-    telefone BIGINT NOT NULL, 
-    rede_social VARCHAR(255) NOT NULL,
-    logo_loja LONGBLOB,
-    paga_pix VARCHAR(255),
-    paga_ted VARCHAR(255) ,
-    paga_boleto VARCHAR(255),
-    paga_dinheiro VARCHAR(255),
-    faixa_preco_ini VARCHAR(255),
-    faixa_preco_fim VARCHAR(255),
-    hora VARCHAR(255),
-    entrega VARCHAR(255),
-    story VARCHAR(255),
-    termo VARCHAR(255),
-    id_usuario int NOT NULL,
+CREATE TABLE brecho (
+    id_brecho int PRIMARY KEY AUTO_INCREMENT,
+    brecho_nome VARCHAR(255) NOT NULL,
+    brecho_endereco VARCHAR(255) NOT NULL,
+    brecho_img LONGBLOB,
+    brecho_rede VARCHAR(255),
+    brecho_contato VARCHAR(255) NOT NULL,
+    brecho_faixa_preco_ini DECIMAL(10,2),
+    brecho_faixa_preco_fim DECIMAL(10,2),
+    brecho_bio VARCHAR(255) NOT NULL,
+    id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
 );
 
--- views/produtos.php
+++ views/produtos.php
@@ -14,114 +14,98 @@ $produtos = array(
 );
 ?>
 
-<section class="header">
-    <!-- First container -->
-    <div class="container-produtos container_solid">
-      <div class="title_wrapper">
-        <h1>GUIA BRECHO </h1>
-      </div>
-    </div>
+<!-- cards produtos -->
 
-    <!-- Second container -->
-    <div class="container-produtos container_image" aria-hidden="true">
-      <div class="title_wrapper">
-        <h1>GUIA BRECHO</h1>
-      </div>
-    </div>
-  </section>
-
-  <!-- cards produtos -->
-
-  <section class="grid-produto">
-    <div class="titulo">
-      <h2>Produtos</h2>
-    </div>
-    <ul class="gallery">
-      <?php foreach ($produtos as $lista) : ?>
-        <li>
-          <div class="card-produtos">
-            <figcaption>
-              <img src="<?= $lista["imagem"] ?>" alt="" width="1000" height="1500" alt="" sizes="(max-width: 1000px) 100vw, 1000px">
-            </figcaption>
-          </div>
-          <div class="info-produto">
-            <h3><?= $lista["nome"] ?></h3>
-            <p style="font-size: .9rem;"><?= $lista["categoria"] ?> / <?= $lista["brecho"] ?></p>
-            <p style="font-size: .7rem; font-weight:200;color:#666565;margin: .7rem 0rem;"><?= $lista["descricao"] ?> </p>
-            <a href="#modal" id="style-2" class="button-produtos button__link" data-replace="Saiba+"><span>R$ <?= $lista["preco"] ?></span></a>
-
-          </div>
-        </li>
-
-      <?php endforeach; ?>
-    </ul>
-
-    <!-- Modal -->
+<section class="grid-produto">
+  <div class="titulo">
+    <h2>Produtos</h2>
+  </div>
+  <ul class="gallery">
     <?php foreach ($produtos as $lista) : ?>
-      <div class="modal-wrapper" id="modal">
-        <div class="modal-body">
-          <div class="img-loja">
-            <img src="<?= $lista["img-loja"] ?>" alt="">
-          </div>
-          <div class="modal-header">
-            <h2 class="heading"><?= $lista["brecho"] ?></h2>
-            <a href="#!" role="button" class="close" aria-label="close this modal">
-              <svg viewBox="0 0 24 24">
-                <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
-              </svg>
-            </a>
-          </div>
-          <ul>
-            <li>Instagram</li>
-            <li>Facebook</li>
-            <li>Whatsapp</li>
-          </ul>
-          
+      <li>
+        <div class="card-produtos">
+          <figcaption>
+            <img src="<?= $lista["imagem"] ?>" alt="" width="1000" height="1500" alt="" sizes="(max-width: 1000px) 100vw, 1000px">
+          </figcaption>
         </div>
-        <a href="#!" class="outside-trigger"></a>
-      </div>
+        <div class="info-produto">
+          <h3><?= $lista["nome"] ?></h3>
+          <p style="font-size: .9rem;"><?= $lista["categoria"] ?> / <?= $lista["brecho"] ?></p>
+          <p style="font-size: .7rem; font-weight:200;color:#666565;margin: .7rem 0rem;"><?= $lista["descricao"] ?> </p>
+          <a href="#modal" id="style-2" class="button-produtos button__link" data-replace="Saiba+"><span>R$ <?= $lista["preco"] ?></span></a>
+
+        </div>
+      </li>
+
     <?php endforeach; ?>
+  </ul>
 
-    <!-- paginacao -->
+  <!-- Modal -->
+  <?php foreach ($produtos as $lista) : ?>
+    <div class="modal-wrapper" id="modal">
+      <div class="modal-body">
+        <div class="img-loja">
+          <img src="<?= $lista["img-loja"] ?>" alt="">
+        </div>
+        <div class="modal-header">
+          <h2 class="heading"><?= $lista["brecho"] ?></h2>
+          <a href="#!" role="button" class="close" aria-label="close this modal">
+            <svg viewBox="0 0 24 24">
+              <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+            </svg>
+          </a>
+        </div>
+        <ul>
+          <li>Instagram</li>
+          <li>Facebook</li>
+          <li>Whatsapp</li>
+        </ul>
 
-    <div class="cont-pag">
-      <ul class="pagination modal-3">
-        <li><a href="#" class="prev">&laquo</a></li>
-        <li><a href="#" class="active">1</a></li>
-        <li> <a href="#">2</a></li>
-        <li> <a href="#">3</a></li>
-        <li> <a href="#">4</a></li>
-        <li><a href="#" class="next">&raquo;</a></li>
+      </div>
+      <a href="#!" class="outside-trigger"></a>
+    </div>
+  <?php endforeach; ?>
+
+  <!-- paginacao -->
+
+  <div class="cont-pag">
+    <ul class="pagination modal-3">
+      <li><a href="#" class="prev">&laquo</a></li>
+      <li><a href="#" class="active">1</a></li>
+      <li> <a href="#">2</a></li>
+      <li> <a href="#">3</a></li>
+      <li> <a href="#">4</a></li>
+      <li><a href="#" class="next">&raquo;</a></li>
+    </ul>
+  </div>
+
+  <!-- sidebar -->
+
+  <aside class="filtro">
+    <h3>FILTRE POR:</h3>
+    <div class="accordion-menu">
+      <ul class="ul">
+        <li class="li">
+          <input type="checkbox" checked>
+          <i class="arrow"></i>
+          <h2 class="h2">Categoria</h2>
+          <p class="p">Roupas</p>
+          <p class="p">Acessórios</p>
+          <p class="p">Calçados</p>
+        </li>
+        <li class="li">
+          <input type="checkbox" checked>
+          <i class="arrow"></i>
+          <h2 class="h2"></i>Brechó</h2>
+          <p class="p"><?= $lista["brecho"] ?></p>
+          <p class="p"><?= $lista["brecho"] ?></p>
+          <p class="p"><?= $lista["brecho"] ?></p>
+        </li>
       </ul>
     </div>
+  </aside>
 
-    <!-- sidebar -->
-
-    <aside class="filtro">
-      <h3>FILTRE POR:</h3>
-      <div class="accordion-menu">
-        <ul class="ul">
-          <li class="li">
-            <input type="checkbox" checked>
-            <i class="arrow"></i>
-            <h2 class="h2">Categoria</h2>
-            <p class="p">Roupas</p>
-            <p class="p">Acessórios</p>
-            <p class="p">Calçados</p>
-          </li>
-          <li class="li">
-            <input type="checkbox" checked>
-            <i class="arrow"></i>
-            <h2 class="h2"></i>Brechó</h2>
-            <p class="p"><?= $lista["brecho"] ?></p>
-            <p class="p"><?= $lista["brecho"] ?></p>
-            <p class="p"><?= $lista["brecho"] ?></p>
-          </li>
-        </ul>
-      </div>
-    </aside>
-
-  </section>
+</section>
 
 
 
