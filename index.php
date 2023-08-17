0cdea18ce8cb6eb4bcc674ece5da2f86941d0e95
darissonsilv <darissonsilva02@gmail.com>
2023-08-17 19:40:57 -0300

    13/07

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
     <link href="/guia_brecho/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/guia_brecho/css/indexhome.css">
-    <link rel="stylesheet" href="/guia_brecho/css/vendor.css">
 <body>
     
              
