1a27c58faa2c5f78fe0100a4d0d5be3b120fbeb8
Felippe Fialho <felippefialho@hotmail.com>
2023-08-17 14:51:01 -0300

    commit

--- models/produto.php
+++ models/produto.php
@@ -1,54 +1,103 @@
-<?php
-session_start();
-require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/db/conexao.php";
+<?php 
+require_once $_SERVER["DOCUMENT_ROOT"] . '/guia_brecho/db/conexao.php';
 
-class Produto{
-
-    public $nome_produto;
+class Produto {
     public $id_produto;
+    public $nome_produto;
     public $descricao;
     public $categoria;
     public $preco;
     public $estoque;
     public $imagem_produto;
+    public $id_loja;
 
-
-    public function __constructor($id_produto=false){
-
+    public function __construct($id_produto = false)
+    {
         if ($id_produto) {
             $this->id_produto = $id_produto;
-            
-            self::carregar();
+            $this->carregar();
         }
     }
-    
-    public function carregar(){
+
+    public function carregar()
+    {
+        $query = "SELECT * FROM produto WHERE id_produto = :id";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':id', $this->id_produto);
+        $stmt->execute();
+
+        $produto = $stmt->fetch();
+        $this->nome_produto = $produto['nome_produto'];
+        $this->descricao = $produto['descricao'];
+        $this->categoria = $produto['categoria'];
+        $this->preco = $produto['preco'];
+        $this->estoque = $produto['estoque'];
+        $this->imagem_produto = $produto['imagem_produto'];
+        $this->id_loja = $produto['id_loja'];
+    }
+
+    public function criar()
+    {
 
         $conexao = Conexao::conectar();
-        $sql = "SELECT id_produto,nome_produto,descricao,categoria,preco,estoque,imagem_produto FROM produto WHERE id_produto = :id_produto";
-        $query = $conexao->prepare($sql);
-        $query->bindValue(':id_produto',$this->id_produto);        
-        $query->execute();
-        $array = $query->fetch();
-
-        $this->nome_produto = $array["nome_produto"];
-        $this->descricao = $array["descricao"];
-        $this->categoria = $array["categoria"];
-        $this->preco=$array["preco"];
-        $this->estoque=$array["estoque"];
-        $this->imagem_produto=$array["imagem_produto"];
+        $query = "INSERT INTO produto (nome_produto, descricao, categoria, preco, imagem_produto, id_loja) VALUES (:nome, :descricao, :cat, :preco, :img, :loja)";
+        
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':nome', $this->nome_produto);
+        $stmt->bindValue(':descricao', $this->descricao);
+        $stmt->bindValue(':cat', $this->categoria);
+        $stmt->bindValue(':preco', $this->preco);
+        $stmt->bindValue(':img', $this->imagem_produto);
+        $stmt->bindValue(':loja', $this->id_loja);
+        $stmt->execute();
+        $this->id_produto = $conexao->lastInsertId();
+        return $this->id_produto;
     }
 
-
-    public function deletar(){
-
-        $conexao = conexao::conectar();
-        $sql = "DELETE FROM produto WHERE id_produto=:id_produto";
-        $delete = $conexao->prepare($sql);
-        $delete->bindValue(':id_produto',$this->id_produto);
-        $delete->execute();
+    public static function listar()
+    {
+        $query = "SELECT p.*, l.nome_loja FROM produto p JOIN loja l ON p.id_loja = l.id_loja";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->execute();
+        $lista = $stmt->fetchAll();
+        return $lista;
     }
-    
+
+    public static function listarId()
+    {
+        $query = "SELECT p.*, l.nome_loja FROM produto p JOIN loja l ON p.id_loja = l.id_loja";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->execute();
+        $lista = $stmt->fetchAll();
+        return $lista;
+    }
+
+    public function editar()
+    {
+        $query = "UPDATE produto SET nome_produto = :nome, descricao = :descricao, categoria = :cat, preco = :preco, estoque = :estoque WHERE id_produto = :id";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(":nome", $this->nome_produto);
+        $stmt->bindValue(":descricao", $this->descricao);
+        $stmt->bindValue(":cat", $this->categoria);
+        $stmt->bindValue(":preco", $this->preco);
+        $stmt->bindValue(":estoque", $this->estoque);
+        $stmt->bindValue(":id", $this->id_produto);
+        $stmt->execute();
+    }
+
+    public function deletar()
+    {
+        $query = "DELETE FROM produto WHERE id_produto = :id";
+        $conexao = Conexao::conectar();
+        $stmt = $conexao->prepare($query);
+        $stmt->bindValue(':id', $this->id_produto);
+        $stmt->execute();
+    }
+
     public static function listarLimiteProdutos($inicio,$limite){
 
         $conexao = conexao::conectar();
@@ -73,38 +122,10 @@ class Produto{
         }
     }
 
-    public function inserir($arq_img_blob){
 
-        $conexao = Conexao::conectar();
-        $sql = $sql = "INSERT INTO produto (nome_produto,descricao,categoria,preco,estoque,imagem_produto) VALUES (:nome_produto,:descricao,:categoria,:preco,:estoque,:imagem_produto)";
-        $insert = $conexao->prepare($sql);
-        $insert->bindParam(':nome_produto',$this->nome_produto);        
-        $insert->bindParam(':descricao',$this->descricao);
-        $insert->bindParam(':categoria',$this->categoria);
-        $insert->bindParam(':preco',$this->preco);
-        $insert->bindParam(':estoque',$this->estoque);
-        $insert->bindParam(':imagem_produto',$arq_img_blob);
-        $insert->execute();
 
-        if ($insert->rowCount() > 0) {            
-
-            echo "<p> Arquivo cadastrado com sucesso!</p>";
-
-        }else {
-
-            echo "<p> Arquivo nao cadastrado!</p>";
-        }
-    }
-
-    public function editar(){
-
-        $conexao = conexao::conectar();
-        $sql = "UPDATE produto SET nome_produto=:nome_produto,descricao=:descricao,categoria=:categoria,preco=:preco,estoque=:estoque,imagem_produto=:imagem_produto WHERE id_produto=:id_produto";
-        $edit = $conexao->prepare($sql);
-        $edit->bindValue(':nome_produto',$this->nome_produto);
-        $edit->bindValue(':descricao',$this->descricao);
-        $edit->bindValue(':categoria',$this->categoria);
-        $edit->bindValue(':id_produto',$this->id_produto);
-        $edit->execute();
-    }
 }
+    
+    
+
+    
