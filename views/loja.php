efa90811173aec2b44fbcf249df05c49434be68f
carolinemorim <134016485+carolinemorim@users.noreply.github.com>
2023-08-17 21:31:57 -0300

    Delete views directory

--- views/loja.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
-?>
-
-
-<?php
-require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
-?>
