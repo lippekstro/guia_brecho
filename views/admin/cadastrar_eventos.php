f0796bef78ae3f7f2b4319a8e014bcf5a1c18c3e
carolinemorim <134016485+carolinemorim@users.noreply.github.com>
2023-08-23 17:08:35 -0300

    Update cadastrar_eventos.php
    correções

--- views/admin/cadastrar_eventos.php
+++ views/admin/cadastrar_eventos.php
@@ -4,45 +4,29 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/cabecalho.php";
 
 <link rel="stylesheet" href="/guia_brecho/css/style1.css">
 
-<form id="form-evento" action="evento_controller.php" method="post" enctype="multipart/form-data">
-  <label for="nome">Titulo do evento:</label>
-  <input type="text" id="nome" name="nome_evento" required>
+<form id="form-evento" action="/guia_brecho/controllers/evento_controller.php" method="post" enctype="multipart/form-data">
+  
+<form action="evento_controller.php" method="post" enctype="multipart/form-data">
+        <label for="nome_evento">Nome do Evento:</label>
+        <input type="text" id="nome_evento" name="nome_evento" required><br><br>
 
-  <label for="data">Data do evento:</label>
-  <input type="date" id="data" name="data_evento" required>
+        <label for="data_evento">Data do Evento:</label>
+        <input type="date" id="data_evento" name="data_evento" required><br><br>
 
-  <label for="hora-cons">Escolha o horário da consulta: </label>
-  <input id="hora-cons" type="time" name="horario" value="00:00" required>
+        <label for="horario">Horário do Evento:</label>
+        <input type="time" id="horario" name="horario" required><br><br>
 
-  <label for="local">Local do evento:</label>
-  <label for="logradouro">Logradouro</label>
-  <input id="logradouro" type="text" name="logradouro" required />
-  <label for="numero">Número</label>
-  <input id="numero" type="text" name="numero" />
-  <label for="complemento"> Complemento</label>
-  <input id="complemento" type="text" name="complemento" />
-  <label for="bairro">Bairro</label>
-  <input id="bairro" type="text" name="bairro" required />
-  <label for="uf">Estado</label>
-  <input id="uf" type="text" name="uf" required />
+        <label for="local_evento">Local do Evento:</label>
+        <input type="text" id="local_evento" name="local_evento" required><br><br>
 
-  <label for="img">Arquivar Imagens</label>
-  <input type="file" name="imagem_evento" />
+        <label for="imagem_evento">Imagem do Evento:</label>
+        <input type="file" id="imagem_evento" name="imagem_evento"><br><br>
 
-  <label for="descricao">Descrição do evento:</label><br>
-  <textarea id="descricao" rows="4" name="descricao_evento" required></textarea>
+        <label for="descricao_evento">Descrição do Evento:</label><br>
+        <textarea id="descricao_evento" name="descricao_evento" rows="4" required></textarea><br><br>
 
-  <button type="submit">Enviar</button>
-</form>
-
-<br>
-<div></div>
-<p id="demo">Clique para obter sua localização:</p>
-<button onclick="getLocation()">Clique aqui</button>
-<div id="mapholder"></div>
-<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
-
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <button type="submit">Cadastrar Evento</button>
+    </form>
 
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/guia_brecho/templates/rodape.php";
