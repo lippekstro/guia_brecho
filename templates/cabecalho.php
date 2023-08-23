03cc5ed5cfb87082632946274800dc0d299cd933
lippekstro <fellipecastro13@hotmail.com>
2023-08-23 09:09:16 -0300

    apaguei imagens

--- templates/cabecalho.php
+++ templates/cabecalho.php
@@ -3,14 +3,14 @@ session_start();
 date_default_timezone_set('America/Fortaleza');
 ?>
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-BR" >
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guia Brechó</title>
-    <link rel="shortcut icon" href="/guia_brecho/img/img.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/guia_brecho/img/favico.ico" type="image/x-icon">
 
     <!-- css bootstrap -->
     <link rel="stylesheet" href="/guia_brecho/css/bootstrap.css">
